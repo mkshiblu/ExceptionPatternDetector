2119db20436ffff66281eb2e3466207ef3816cb5
Mehran Jodavi <m_jodavi@encs.concordia.ca>
2020-03-04 13:32:21 -0400

    #6: changed to have one example of wrapping which is not destructive

--- test_data/SimpleTests/src/overCatchTest/DestructiveWrappingTest.java
+++ test_data/SimpleTests/src/overCatchTest/DestructiveWrappingTest.java
@@ -5,36 +5,37 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.management.modelmbean.XMLParseException;
+
 public class DestructiveWrappingTest {
-	
+
 	public void A() throws OtherException {
 		try {
 			throw new FileNotFoundException();
-		}catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			throw new OtherException(e.getMessage());
 		}
 	}
-	
+
 //	if handling both checked and unchecked Exception, only consider the throw inside catch block
-	
+
 	public void B() throws FileNotFoundException {
-//		must use throws FileNotFoundException because it is checked exception
 		try {
-			
-		}catch(Exception e) {
+		} catch (Exception e) {
 			throw new FileNotFoundException(e.getMessage());
 		}
+//		must use throws FileNotFoundException because it is checked exception
+
 	}
-	
-	public void C() {
+
+	public void C() throws XMLParseException {
 		try {
 //			unchecked exception does not need to be declared in the method declartion with throws
-		}catch(Exception e) {
-			throw new NullPointerException(e.getMessage());
+		} catch (Exception e) {
+			throw new XMLParseException(e,e.getMessage());
 		}
 	}
 
-	
 //	public void D() {
 //		try {
 //		
