196b7f4f26702113f505c1c10d3b13d622929f6c
Mehran Jodavi <m_jodavi@encs.concordia.ca>
2020-03-05 09:52:38 -0400

    Removed some unused code

--- src/jeaphunter/JeapHunterProject.java
+++ src/jeaphunter/JeapHunterProject.java
@@ -1,54 +1,50 @@
 package jeaphunter;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jdt.core.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import org.eclipse.core.resources.IProject;
-import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
 
 /**
  * Abstraction and wrapper around the JDT project
  */
 public class JeapHunterProject {
 
-	private IProject project;
+    private IProject project;
 
-	public JeapHunterProject(IProject project) {
-		this.project = project;
-	}
+    public JeapHunterProject(IProject project) {
+        this.project = project;
+    }
 
-	/**
-	 * Returns all the java compilation units of this project
-	 */
-	public ICompilationUnit[] getSourceFiles() throws JavaModelException {
-		List<ICompilationUnit> files = new ArrayList<>();
-		for (IPackageFragment packageFragment : JavaCore.create(project).getPackageFragments()) {
-			if (packageFragment.getKind() == IPackageFragmentRoot.K_SOURCE) {
-				// Get all the source file / compilation unit of the package and add to the list
-				Arrays.stream(packageFragment.getCompilationUnits()).forEach(icompUnit -> {
-					files.add(icompUnit);
-				});
-			}
-		}
-		return files.toArray(new ICompilationUnit[0]);
-	}
+    /**
+     * Returns all the java compilation units of this project
+     */
+    public ICompilationUnit[] getSourceFiles() throws JavaModelException {
+        List<ICompilationUnit> files = new ArrayList<>();
+        for (IPackageFragment packageFragment : JavaCore.create(project).getPackageFragments()) {
+            if (packageFragment.getKind() == IPackageFragmentRoot.K_SOURCE) {
+                // Get all the source file / compilation unit of the package and add to the list
+                Arrays.stream(packageFragment.getCompilationUnits()).forEach(icompUnit -> {
+                    files.add(icompUnit);
+                });
+            }
+        }
+        return files.toArray(new ICompilationUnit[0]);
+    }
 
-	/**
-	 * Getter for the underlying project
-	 */
-	public IProject getProject() {
-		return project;
-	}
-	
-	/**
-	 * Returns the name of the project
-	 */
-	public String getName() {
-		return project.getName();
-	}
+    /**
+     * Getter for the underlying project
+     */
+    public IProject getProject() {
+        return project;
+    }
+
+    /**
+     * Returns the name of the project
+     */
+    public String getName() {
+        return project.getName();
+    }
 }
